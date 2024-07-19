98e4f7bbc0fcc944ffefce0dd84e8eb87bef13d8
Reinmmar <31139601+TimeStepYT@users.noreply.github.com>
2024-07-19 23:09:59 +0200

    Initial Commit

---
+++ include/includes.hpp
@@ -0,0 +1,9 @@
+#include <Geode/Geode.hpp>
+
+#include <Geode/binding/DailyLevelPage.hpp>
+#include <Geode/binding/FLAlertLayer.hpp>
+#include <Geode/modify/DailyLevelPage.hpp>
+#include <Geode/modify/DailyLevelNode.hpp>
+#include <Geode/modify/CreatorLayer.hpp>
+
+extern bool atEvent;
